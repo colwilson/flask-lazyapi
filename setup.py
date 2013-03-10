062db24771dde961bd0c7d0cf6009aac4bb55ea8
Terse Col <tersecol@gmail.com>
2013-03-10 11:09:30 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+import os
 from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='flask-lazyapi',
-    version='0.3',
+    version='0.3a',
     url='https://github.com/colwilson/flask-lazyapi',
     license='BSD',
     author='Col Wilson',
     author_email='colwilson@bcs.org',
     description='A Simple, Restful MongoDB Server built on Flask and Flask-Classy',
-    long_description=__doc__,
+    long_description=read('README.md'),
     py_modules=['flask_lazyapi'],
     zip_safe=False,
     include_package_data=True,
@@ -18,6 +23,13 @@ setup(
         'Flask-Classy',
         'pymongo'
     ],
-    test_suite='test_lazyapi'
+    test_suite='test_lazyapi',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Database :: Front-Ends'        
+        ]
+    
 )
 
